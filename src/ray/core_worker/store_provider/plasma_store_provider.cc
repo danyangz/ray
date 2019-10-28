10f10e4c5065d95cea56cfb03bff320f79be4109
Danyang Zhuo <danyangzhuo@gmail.com>
2019-10-28 07:37:10 +0000

    Add logging at worker level

--- src/ray/core_worker/store_provider/plasma_store_provider.cc
+++ src/ray/core_worker/store_provider/plasma_store_provider.cc
@@ -86,6 +86,8 @@ Status CoreWorkerPlasmaStoreProvider::FetchAndGetFromPlasmaStore(
     int64_t timeout_ms, bool fetch_only, const TaskID &task_id,
     std::unordered_map<ObjectID, std::shared_ptr<RayObject>> *results,
     bool *got_exception) {
+  RAY_LOG(WARNING) << "fetch and get";
+
   RAY_RETURN_NOT_OK(raylet_client_->FetchOrReconstruct(batch_ids, fetch_only, task_id));
 
   std::vector<plasma::ObjectID> plasma_batch_ids;
